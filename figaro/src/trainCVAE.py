7e94da3062b7c243f177d5e11aaf09cf781fa181
DKomnick <david.komnick@uni-bielefeld.de>
2023-09-13 12:48:12 +0200

    minor changes in the VAE training loop for book keeping

--- figaro/src/trainCVAE.py
+++ figaro/src/trainCVAE.py
@@ -8,6 +8,8 @@ from torch.utils.data import DataLoader
 dataset = EncoderDataSet("./samples/encoder_hidden")
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+save_name = "vae_hidden"
+
 # model parameter
 encoding_size = [512, 128, 32, 16]
 decoding_size = [16, 32, 128, 512]
@@ -19,13 +21,12 @@ model.to(device)
 
 # each item has about 200 bars. In one epoch only one bar is addressed. Within 2000 epochs we
 # see each bar only once
-epochs = 2000
+epochs = 20
 lr = 0.001
 batch_size = 16
 optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
 data_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True)
-
 
 # loss
 def loss_fn(recon_x, x, mean, log_var):
@@ -35,8 +36,10 @@ def loss_fn(recon_x, x, mean, log_var):
 
     return (BCE + KLD) / x.size(0)
 
+loss_training = []
 
 for epoch in range(epochs):
+    loss_epoch = []
     for iteration, (x, y) in enumerate(data_loader):
         x, y = x.to(device), y.to(device)
 
@@ -44,6 +47,16 @@ for epoch in range(epochs):
 
         loss = loss_fn(recon_x, x, mean, log_var)
 
+        loss_epoch.append(loss.item())
+
         optimizer.zero_grad()
         loss.backward()
+
         optimizer.step()
+    loss_training.append(loss_epoch)
+
+loss_arr = np.array(loss_training)
+with open(f'{save_name}_loss.npy', 'wb') as f:
+    np.save(f, loss_arr)
+
+torch.save(model.state_dict(), f'{save_name}_loss.npy')
